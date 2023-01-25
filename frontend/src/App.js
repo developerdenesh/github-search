69f917daa427ab9afbcad5be95e46ecd8013eba3
Denesh <denesh@u.nus.edu>
2023-01-26 02:44:58 +0800

    Adding ReactJS frontend to the stack

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Input from './components/input';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Github Search
+        </p>
+        <Input />
+      </header>
+    </div>
+  );
+}
+
+export default App;
