76a4f5782fb54e8fe0c1c1d5fe8a9317719346a4
Denesh <denesh@u.nus.edu>
2023-01-27 00:42:48 +0800

    Added radio buttons to select the type of query to be  made to the backend

--- frontend/src/components/input.js
+++ frontend/src/components/input.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { Input, ListItem, ListIcon, List } from "@chakra-ui/react";
+import { Input, ListItem, ListIcon, List, Radio, RadioGroup, Stack, Spinner } from "@chakra-ui/react";
 import { StarIcon } from '@chakra-ui/icons'
 import axios from "axios";
 
 const Searchinput = () => {
     const [value, setValue] = useState("")
+    const [keyword, setKeyword] = useState("users")
     const [apitimer, setapitimer] = useState(null)
     const [urllist, setUrllist] = useState([])
+    const [placeholdertext, setPlaceholdertext] = useState("Enter the search query")
+    const [displayspinner, setDisplayspinner] = useState(false)
 
     useEffect(() => {
         setapitimer(
@@ -16,12 +19,50 @@ const Searchinput = () => {
         )
     }, [])
 
+
+    useEffect(() => {
+        if (keyword === "issues") {
+            setPlaceholdertext("windows+label:bug+language:python+state:open&sort=created&order=asc")
+        } else if (keyword === "repositories") {
+            setPlaceholdertext("typescript-twilio")
+        } else if (keyword === "topics") {
+            setPlaceholdertext("typescript")
+        } else if (keyword === "labels") {
+            setPlaceholdertext("bug+defect+enhancement&repository_id=64778136")
+        } else if (keyword === "commits") {
+            setPlaceholdertext("q=repo:developerdenesh/typescript-twilio+css")
+        } else if (keyword === "code") {
+            setPlaceholdertext("q=addClass+in:file+language:js+repo:jquery/jquery")
+        } else if (keyword === "users") {
+            setPlaceholdertext("developerdenesh")
+        }
+
+        // Reset the input box once the radio has been clicked
+        setValue("")
+        setUrllist([])
+    }, [keyword])
+
+
+    const callApiManager = ({ query }) => {
+        // It is not caputuring the last letter
+        clearTimeout(apitimer)
+
+        setapitimer(
+            setTimeout(() => {
+                // Clear the text field when a new suggestion is being queried
+                setUrllist([])
+                callApi({ query: query })
+            }, 1000)
+        )
+    }
+
     const callApi = ({ query }) => {
         const data = {
             query: query,
-            type: "repositories"
+            type: keyword
         }
 
+        setDisplayspinner(true)
         axios({
             // Endpoint to send files
             url: "http://localhost:5000/search",
@@ -36,17 +77,30 @@ const Searchinput = () => {
             .then((res) => {
                 console.log(res.data)
                 res.data.map((element) => {
-                    let temp_json = {
-                        id: element.id,
-                        url: element.url,
+                    let temp_json;
+                    if (keyword === "topics" || keyword === "code") {
+                        console.log(element)
+                        temp_json = {
+                            id: element.url,
+                            url: element.url,
+                        }
+                    } else {
+                        temp_json = {
+                            id: element.id,
+                            url: element.url,
+                        }
                     }
                     // Display data on the screen
                     setUrllist(urllist => [...urllist, temp_json])
                     return 0;
                 })
+                
+                setDisplayspinner(false)
+                console.log(urllist)
 
             })
             .catch((err) => {
+                setDisplayspinner(false)
                 console.log(err);
             });
     }
@@ -56,60 +110,45 @@ const Searchinput = () => {
 
         // This seems to lag such that the api call is unable to use value
         setValue(event.target.value)
-
-        // It is not caputuring the last letter
-        clearTimeout(apitimer)
-
-        setapitimer(
-            setTimeout(() => {
-                // Clear the text field when a new suggestion is being queried
-                setUrllist([])
-                callApi({ query: event.target.value })
-            }, 1000)
-        )
+        callApiManager({ query: event.target.value })
     }
 
     const handleKeyDown = (event) => {
-        // const keycode = event.keyCode
-        // if (event.key === "Enter") {
-        //     return;
-        // }
-
-        // let temp_value = value;
-        // if (event.key === "Backspace" && temp_value.length > 0) {
-        //     temp_value = temp_value.slice(0, -1);
-        // } else {
-
-        //     const valid = 
-        //     (keycode > 47 && keycode < 58)   || // number keys
-        //     keycode === 32 || keycode === 13   || // spacebar & return key(s) (if you want to allow carriage returns)
-        //     (keycode > 64 && keycode < 91)   || // letter keys
-        //     (keycode > 95 && keycode < 112)  || // numpad keys
-        //     (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
-        //     (keycode > 218 && keycode < 223);   // [\]' (in order)
-
-        //     if (valid) {
-        //         temp_value += event.key
-        //     }
-        // }
-
-        // setValue(temp_value)
-        // console.log(temp_value)
+        if (event.keyCode === 39) {
+            setValue(placeholdertext)
+            callApiManager({ query: placeholdertext })
+        }
     }
 
 
     return (
         <>
+            <RadioGroup onChange={setKeyword} value={keyword}>
+                <Stack direction='row'>
+                    <Radio value='code'>Code</Radio>
+                    <Radio value='commits'>Commits</Radio>
+                    <Radio value='issues'>Issues and Pull Requests</Radio>
+                    <Radio value='labels'>Labels</Radio>
+                    <Radio value='repositories'>Repositories</Radio>
+                    <Radio value='topics'>Topics</Radio>
+                    <Radio value='users'>Users</Radio>
+                </Stack>
+            </RadioGroup>
+
             <Input
                 value={value}
                 onChange={handleChange}
                 onKeyDown={handleKeyDown}
-                placeholder="Enter the search query"
+                placeholder={placeholdertext}
+                className="search-bar"
                 size='lg'
             />
+
+            {displayspinner ? <Spinner color='red.500' /> : <></>}
+
             <List>
                 {urllist.map((item) => (
-                    <ListItem key={item.id}>
+                    <ListItem key={item.id} width={1100}>
                         <ListIcon as={StarIcon} color='green.500' />
                         {item.url}
                     </ListItem>
