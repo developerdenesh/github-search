6248e79730b7a16f6f40d656a175bd3f3e655ca0
Denesh <denesh@u.nus.edu>
2023-01-26 17:39:27 +0800

    Front: Adding a timer to prevent to many requests being sent

--- frontend/src/components/input.js
+++ frontend/src/components/input.js
@@ -1,56 +1,99 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Input } from "@chakra-ui/react";
 import axios from "axios";
 
-
 const Searchinput = () => {
     const [value, setValue] = useState("")
+    const [apitimer, setapitimer] = useState(null)
+
+    useEffect(() => {
+        setapitimer(
+            setTimeout(() => {
+                console.log("timer initialised")
+            }, 1000)
+        )
+    }, [])
+
+    const callApi = ({ query }) => {
+        const data = {
+            query: query,
+            type: "repositories"
+        }
+
+        axios({
+            // Endpoint to send files
+            url: "http://localhost:5000/search",
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+            },
+
+            // Attaching the form data
+            data: data,
+        })
+            .then((res) => {
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.error(err)
+            });
+    }
 
     const handleChange = (event) => {
+        console.log(event.target.value)
+
+        // This seems to lag such that the api call is unable to use value
         setValue(event.target.value)
+
+        // It is not caputuring the last letter
+        clearTimeout(apitimer)
+
+        setapitimer(
+            setTimeout(() => {
+                callApi({ query: event.target.value })
+            }, 2000)
+        )
     }
 
     const handleKeyDown = (event) => {
-        if (event.key === "Enter") {
-            // TODO: make this a component
-            let formData = {
-                body: "name",
-                type: "user"
-            }
-    
-            axios({
+        // const keycode = event.keyCode
+        // if (event.key === "Enter") {
+        //     return;
+        // }
 
-                // Endpoint to send files
-                url: "http://localhost:5000/search",
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json;charset=UTF-8',
-                    "Access-Control-Allow-Origin": "*",
-                    'Access-Control-Allow-Credentials': true
-                },
+        // let temp_value = value;
+        // if (event.key === "Backspace" && temp_value.length > 0) {
+        //     temp_value = temp_value.slice(0, -1);
+        // } else {
 
-                // Attaching the form data
-                data: JSON.stringify(formData),
-            })
-                .then((res) => {
-                    console.log(res)
-                })
-                .catch((err) => {
-                    console.error(err)
-                });
-        }
-        console.log("enter")
+        //     const valid = 
+        //     (keycode > 47 && keycode < 58)   || // number keys
+        //     keycode === 32 || keycode === 13   || // spacebar & return key(s) (if you want to allow carriage returns)
+        //     (keycode > 64 && keycode < 91)   || // letter keys
+        //     (keycode > 95 && keycode < 112)  || // numpad keys
+        //     (keycode > 185 && keycode < 193) || // ;=,-./` (in order)
+        //     (keycode > 218 && keycode < 223);   // [\]' (in order)
+            
+        //     if (valid) {
+        //         temp_value += event.key
+        //     }
+        // }
+
+        // setValue(temp_value)
+        // console.log(temp_value)
     }
 
 
     return (
-        <Input
-            value={value}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-            placeholder="Enter the search query"
-            size='lg'
-        />
+        <>
+            <Input
+                value={value}
+                onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                placeholder="Enter the search query"
+                size='lg'
+            />
+        </>
     )
 }
 
